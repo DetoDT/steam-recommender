512c80fde05fbe16a2e80c9fde22a30127285eb4
Francesco De Tommaso <francescodeto@proton.me>
2026-03-19 23:10:18 +0100

    Finalized preprocessing, it now only needs to be refactored in a clean structure, created EDA notebook and started looking into eda

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,86 +1,116 @@
 import pandas as pd
 import numpy as np
+import time
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 RAW_DATA_FOLDER = PROJECT_ROOT / "data" / "raw" 
 OUTPUT_FOLDER = PROJECT_ROOT / "data" / "processed"
 
-users_df = pd.read_csv(RAW_DATA_FOLDER / "users_dummy.csv")
-games_df = pd.read_csv(RAW_DATA_FOLDER / "games_dummy.csv")
-recommendations_df = pd.read_csv(RAW_DATA_FOLDER / "recommendations_dummy.csv")
 
-output_path = '../data/processed/' 
-
-###
-# DATA CLEANING
-###
-
-## dropping duplicates
-games_df = games_df.drop_duplicates()
-users_df = users_df.drop_duplicates()
-recommendations_df = recommendations_df.drop_duplicates()
-
-## Replace missing/wrong date with a sentinel value
-games_df['date_release'] = pd.to_datetime(games_df['date_release'], format='%Y-%m-%d', errors='coerce')
-games_df["date_release"] = games_df["date_release"].fillna(pd.Timestamp("1900-01-01"))
+if any(OUTPUT_FOLDER.iterdir()):
+    print("Make sure output directory (data/processed) is empty before proceding.")
+    exit()
 
 
-## win, mac, linux, steam_deck missing or not boolean
-cols = ["win", "mac", "linux", "steam_deck"]
-games_df[cols] = games_df[cols].fillna(False)
-games_df[cols] = games_df[cols].where(games_df[cols].isin([True, False]), False)
+try:
+    start_time = time.time()
+    print("DATA PREPROCESSING\n")
 
-## Notify if popular game contiains missing fields
 
-mask = (games_df['user_reviews'] >= 10000) & (games_df.isna().any(axis=1))
-problem_rows = games_df.loc[mask]
-problem_rows.to_csv(OUTPUT_FOLDER / "missing_high_review_entries.log", index=False)
+    print("> Loading dataset...")
+    users_df = pd.read_csv(RAW_DATA_FOLDER / "users.csv")
+    games_df = pd.read_csv(RAW_DATA_FOLDER / "games.csv")
+    recommendations_df = pd.read_csv(RAW_DATA_FOLDER / "recommendations.csv")
 
-## Deleting entries with missing fields
+    ###
+    # DATA CLEANING
+    ###
 
-print(games_df.isna().sum())
-print()
-if input("Drop values? [y/N]") == 'y':
+    ## dropping duplicates
+    print("> Dropping duplicates...")
+    games_df = games_df.drop_duplicates()
+    users_df = users_df.drop_duplicates()
+    recommendations_df = recommendations_df.drop_duplicates()
+
+    ## Replace missing/wrong date with a sentinel value
+    print("> Fixing fields...")
+    games_df['date_release'] = pd.to_datetime(games_df['date_release'], format='%Y-%m-%d', errors='coerce')
+    games_df["date_release"] = games_df["date_release"].fillna(pd.Timestamp("1900-01-01"))
+
+
+    ## win, mac, linux, steam_deck missing or not boolean
+    cols = ["win", "mac", "linux", "steam_deck"]
+    games_df[cols] = games_df[cols].fillna(False)
+    games_df[cols] = games_df[cols].where(games_df[cols].isin([True, False]), False)
+
+    ## Notify if popular game contiains missing fields
+
+    mask = (games_df['user_reviews'] >= 10000) & (games_df.isna().any(axis=1))
+    problem_rows = games_df.loc[mask]
+
+    ## Deleting entries with missing fields
+
+    # print(games_df.isna().sum())
+    # print()
+    # if input("Drop values? [y/N]") == 'y':
+    #     games_df = games_df.dropna()
+
     games_df = games_df.dropna()
+    ## Transform Steam rating into a number
 
-## Transform Steam rating into a number
+    rating_map = {
+        "Very Negative": 0,
+        "Negative": 1,
+        "Mostly Negative": 2,
+        "Mixed": 3,
+        "Mostly Positive": 4,
+        "Positive": 5,
+        "Very Positive": 6,
+        "Overwhelmingly Positive": 7
+    }
 
-rating_map = {
-    "Very Negative": 0,
-    "Negative": 1,
-    "Mostly Negative": 2,
-    "Mixed": 3,
-    "Mostly Positive": 4,
-    "Positive": 5,
-    "Very Positive": 6,
-    "Overwhelmingly Positive": 7
-}
+    games_df["rating_score"] = games_df["rating"].map(rating_map)
 
-games_df["rating_score"] = games_df["rating"].map(rating_map)
+    ## Deleting entries containing missing values for the other files
 
-## Deleting entries containing missing values for the other files
-
-print(users_df.isna().sum())
-print()
-if input("Drop values? [y/N]") == 'y':
+    # print(users_df.isna().sum())
+    # print()
+    # if input("Drop values? [y/N]") == 'y':
+    #     users_df = users_df.dropna()
+    # print(recommendations_df.isna().sum())
+    # print()
+    # if (input("Drop values? [y/N]") == 'y'):
+    #     recommendations_df = recommendations_df.dropna()
+    
     users_df = users_df.dropna()
-print(recommendations_df.isna().sum())
-print()
-if (input("Drop values? [y/N]") == 'y'):
     recommendations_df = recommendations_df.dropna()
 
-## remove games with less than 100 reviews
-games_df= games_df[games_df['user_reviews'] >= 100]
+    ## remove games with less than 100 reviews
+    games_df= games_df[games_df['user_reviews'] >= 100]
 
-## remove users with less than 5 products
-users_df = users_df[users_df['products'] >= 5]
+    ## remove users with less than 5 products
+    users_df = users_df[users_df['products'] >= 5]
 
-## remove from recommendations all entries that contain a gameid / userid not present in the other files
-recommendations_df = recommendations_df.merge(users_df[['user_id']], on='user_id')
-recommendations_df = recommendations_df.merge(games_df[['app_id']], on='app_id')
+    ## remove from recommendations all entries that contain a gameid / userid not present in the other files
+    recommendations_df = recommendations_df.merge(users_df[['user_id']], on='user_id')
+    recommendations_df = recommendations_df.merge(games_df[['app_id']], on='app_id')
 
-## export
-users_df.to_csv(OUTPUT_FOLDER / 'users_dummy.csv')
-games_df.to_csv(OUTPUT_FOLDER / 'games_dummy.csv')
-recommendations_df.to_csv(OUTPUT_FOLDER / 'recommendations_dummy.csv')
+    ## export
+    print("> Exporting final files...")
+    users_df.to_csv(OUTPUT_FOLDER / 'users.csv')
+    games_df.to_csv(OUTPUT_FOLDER / 'games.csv')
+    recommendations_df.to_csv(OUTPUT_FOLDER / 'recommendations.csv')
+
+    if (len(problem_rows) > 1):
+        problem_rows.to_csv(OUTPUT_FOLDER / "missing_high_review_entries.log", index=False)
+
+    end_time = time.time()
+
+    print("\n DONE! All the processed csv have been saved under data/processed")
+    print(f'Total time: {int(end_time-start_time)} seconds.')
+    
+except Exception as err:
+    print(f'\nUnexpected error, quitting program ({err})')
+    exit()
+
